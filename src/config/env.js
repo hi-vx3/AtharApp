96a449eb240a334010216d12f785d590a69bcc6c
Bassm <bassm6xx@gmail.com>
2026-07-01 03:21:24 +0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 const ENV = {
   // مسار الواجهة الخلفية (Backend) الأساسي
-  API_BASE_URL: process.env.REACT_APP_API_BASE || 'http://localhost:5008',
-
+  API_BASE_URL: process.env.REACT_APP_API_BASE || 'https://api.atharhub.dev',
+  // Hiii im basm
   // معرف العميل الخاص بـ GitHub App (Client ID)
   GITHUB_CLIENT_ID: process.env.REACT_APP_GITHUB_CLIENT_ID,
 };
